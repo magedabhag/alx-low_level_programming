b5d560043b5bff73c70d9252d07dc10180bb2580
magedabhag <magedabhagnagy@gmail.com>
2023-08-12 19:52:23 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * Description: A c program that prints with put function
  *
  * Return: Always 0 (Success)
- */_
+*/
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
