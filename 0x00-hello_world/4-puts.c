fd1a1f5c1ef10d7dbb0d8657dcc136b288994923
magedabhag <magedabhagnagy@gmail.com>
2023-08-12 19:48:21 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */_
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
