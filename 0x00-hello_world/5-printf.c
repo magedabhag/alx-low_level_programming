c0560b58c455938b4f43dcc9506c123844dc1e28
magedabhag <magedabhagnagy@gmail.com>
2023-08-12 20:18:47 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * decription: a c program that prints with printf function.
+ *
+ * return: always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
