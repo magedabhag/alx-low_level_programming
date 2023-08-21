97b32da88e202ee9a7e37cccddb1e7a87e8a9b97
magedabhag <magedabhagnagy@gmail.com>
2023-08-20 17:52:29 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char  ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
