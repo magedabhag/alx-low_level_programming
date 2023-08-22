c25fdc072a3cf34ae35bfc07a21383a2a581c283
magedabhag <magedabhagnagy@gmail.com>
2023-08-21 21:21:30 -0700

    a

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	int digit = 48;
+	int digit = 48; /*48; decimal rep of 0*/
 
 	while (ch <= 102)
 	{
 		putchar(digit);
+
+		/* after 9 we jump till 96; `**/
 		if (digit == 57)
 			digit += 39;
 		digit++;
