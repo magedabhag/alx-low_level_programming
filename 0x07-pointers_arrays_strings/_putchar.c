fed578c8b92aa11aba5990abfddd63bdad8a8ea7
magedabhag <magedabhagnagy@gmail.com>
2023-08-28 18:48:00 -0700

    a

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.n>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on seccces 1.
+ * on error, -1 is returned, and error is set.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
