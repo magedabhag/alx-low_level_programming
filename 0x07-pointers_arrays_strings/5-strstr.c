6e06806500307eb7d319fb5ecd5241a39c6d04ce
magedabhag <magedabhagnagy@gmail.com>
2023-08-28 22:00:35 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,31 @@
 #include "main.h"
+
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: always 0
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in hystack
+ * Return: return 0
+ *
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
 	{
-		char *l = haystack;
-		char *p = needle;
+		str1 = haystack; /*values*/
+		str2 = needle;
 
-		while (*l == *p && *p != '\0')
+		/*star WHILE*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
-			l++;
-			P++;
+			haystack++;
+			str2++;
 		}
-
-		if (*p == '\0')
-			return (haystack);
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
 	}
-
 	return (0);
 }
