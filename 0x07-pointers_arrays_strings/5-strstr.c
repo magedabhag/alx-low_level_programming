2dcb96c210f562fb28f95d0f065821feac361c69
magedabhag <magedabhagnagy@gmail.com>
2023-08-28 20:13:15 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * _strstr - Entry point
  * @haystack: input
  * @needle: input
  * Return: always 0
-*/
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
