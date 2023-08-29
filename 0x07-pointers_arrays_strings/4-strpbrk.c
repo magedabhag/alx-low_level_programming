d7344c21fa7371cc4d948670a202ab59c3161426
magedabhag <magedabhagnagy@gmail.com>
2023-08-28 19:30:58 -0700

    a

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - copies memory area
+ * @s: memory area
+ * @accept: sourse
+ *
+ * Return: the pointer to dest.
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (jtr = 0; accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+	return (0);
+}
