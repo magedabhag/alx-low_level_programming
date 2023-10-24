87bf6a37ee00288db9f5e6f1042056b2e000ad82
magedabhag <magedabhagnagy@gmail.com>
2023-10-24 15:04:26 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,18 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-void reset_to_98(int *);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
 
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
-}
+#endif /*Bwave Bright*/
